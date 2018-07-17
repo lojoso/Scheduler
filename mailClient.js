263a90ae6e6ab1ffc89fe2cd85bc252e6c1655e8
lojoso <lujinshen@daqo.com>
2018-07-17 13:35:16 +0800

    添加html图片的嵌入方式

--- mailClient.js
+++ mailClient.js
@@ -6,11 +6,11 @@ module.exports = (body) => {
     // setup email data with unicode symbols
     let mailOptions = {
         from: '"邮件发送测试" <daqo_gitlab@163.com>', // sender address
-        to: 'suewangyihe0@163.com', // list of receivers
+        to: '1399912028@qq.com', // list of receivers
         subject: '数据分析周报', // Subject line
         text: '请查看附件', // plain text body
-        html: '' ,// html body
-        attachments:body.attachments
+        html: '<img src="cid:img"/>' ,// html body
+        attachments:body.attachments.concat([{filename:"其他.jpg",path:"./output/其他.jpg",cid:"img"}])
     };
 
     // send mail with defined transport object
